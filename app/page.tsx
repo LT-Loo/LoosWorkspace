8e34026bb9f965f1297b8d038025d87d6da278bf
LT Loo <lertheng@gmail.com>
2024-01-13 02:32:26 +1100

    changed font

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
 // Home Page
 
-import {promises as fs} from 'fs';
-import localFont from 'next/font/local';
-
-const zapfino = localFont({ src: '../public/fonts/Zapfino.ttf'});
-
 export default async function Home() {
 
   return (
     <div id = "home" className = "container-fluid text-center position-relative">
       <div className = "position-absolute start-50 top-50 translate-middle w-100">
-        <h1 id = "web-title" className = { zapfino.className }>Loo&apos;s Workspace</h1>
+        <h1 id = "web-title">Loo&apos;s Workspace</h1>
         <p id = "intro" className = "mb-4 fs-5">
           Hello! This is <b>LOO</b><br/>
           Computer Science graduate<br/>
